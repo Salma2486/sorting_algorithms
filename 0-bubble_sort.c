bd74c6a114dfeeaa899b8a058db2c8e86d099080
Salma2486 <saloom_001@hotmail.com>
2023-12-14 07:12:46 -0800

    task0

---
+++ 0-bubble_sort.c
@@ -0,0 +1,25 @@
+#include "sort.h"
+/**
+ *bubble_sort - This is the entry point of the code
+ *@array: er gt eg
+ *@size: ytrs ey
+ *Return:0 Success
+ */
+void bubble_sort(int *array, size_t size)
+{
+	int i, j, tmp;
+
+	for (i = 0; i < size; i++)
+	{
+		for (j = 0; j < size - 1; j++)
+		{
+			if (array[j] > array[j + 1])
+			{
+				tmp = array[j];
+				array[j] = array[j + 1];
+				array[j + 1] = tmp;
+				print_array(array, size);
+			}
+		}
+	}
+}
