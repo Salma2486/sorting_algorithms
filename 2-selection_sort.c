090fd26bcd9e6ccb2ec8054248805a80e04d5661
Salma2486 <saloom_001@hotmail.com>
2023-12-15 08:21:46 -0800

    saedf

--- 2-selection_sort.c
+++ 2-selection_sort.c
@@ -7,25 +7,25 @@
  */
 void selection_sort(int *array, size_t size)
 {
-	size_t i, j, min;
-	int tmp;
+	size_t i, j, min_idx;
+	int temp;
 
 	for (i = 0; i < size - 1; i++)
 	{
-		min = i;
+		min_idx = i;
 		for (j = i + 1; j < size; j++)
-
-			if (array[j] < array[min])
-			{
-				min = j;
-			}
-		if (min != i)
 		{
-			tmp = array[i];
-			array[i] = array[min];
-			array[min] = tmp;
+			if (array[j] < array[min_idx])
+				min_idx = j;
 		}
-		print_array(array, size);
-	}
 
+		if (min_idx != i)
+		{
+			temp = array[i];
+			array[i] = array[min_idx];
+			array[min_idx] = temp;
+
+			print_array(array, size);
+		}
+	}
 }
