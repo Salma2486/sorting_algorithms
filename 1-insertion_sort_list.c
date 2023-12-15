058cfed10e5272a399a450f25d3600413229b59c
Salma2486 <saloom_001@hotmail.com>
2023-12-15 05:08:54 -0800

    task01

--- 1-insertion_sort_list.c
+++ 1-insertion_sort_list.c
@@ -7,10 +7,10 @@
  */
 void insertion_sort_list(listint_t **list)
 {
+	listint_t *current, *next, *temp;
+
 	if (list == NULL || *list == NULL || (*list)->next == NULL)
 		return;
-
-	listint_t *current, *next, *temp;
 
 	current = (*list)->next;
 
