ecdc72e38ab0f44c49f7c83f017e0333de49efd9
Salma2486 <saloom_001@hotmail.com>
2023-12-19 13:53:48 -0800

    task9

--- sort.h
+++ sort.h
@@ -23,4 +23,7 @@ void insertion_sort_list(listint_t **list);
 void selection_sort(int *array, size_t size);
 void quick_sort(int *array, size_t size);
 void shell_sort(int *array, size_t size);
+void radix_sort(int *array, size_t size);
+int getMax(const int *array, size_t size);
+void radix_counting_sort(int *array, size_t size, int exp, int *ptr);
 #endif
