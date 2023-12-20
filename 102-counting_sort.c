76d79f4a340c7bce7a95c62880c1db189a916833
Younisprogramming <younistaher12341@gmail.com>
2023-12-19 16:36:45 -0800

    task 102

--- 102-counting_sort.c
+++ 102-counting_sort.c
@@ -7,36 +7,39 @@
  */
 void counting_sort(int *array, size_t size)
 {
+	int max_value = array[0], j;
+	size_t i;
+	int *count_array;
+	int *sorted_arr;
+
 	if (!array || size <= 1)
 		return;
 
-	int max_value = array[0];
-
-	for (size_t i = 1; i < size; i++)
+	for (i = 1; i < size; i++)
 	{
 		if (array[i] > max_value)
 			max_value = array[i];
 	}
 
-	int *count_array = calloc(max_value + 1, sizeof(int));
+	count_array = calloc(max_value + 1, sizeof(int));
 
-	for (size_t i = 0; i < size; i++)
+	for (i = 0; i < size; i++)
 	{
 		count_array[array[i]]++;
 	}
 
-	for (int i = 1; i <= max_value; i++)
+	for (j = 1; j <= max_value; j++)
 	{
-		count_array[i] += count_array[i - 1];
+		count_array[j] += count_array[j - 1];
 	}
-	int *sorted_arr = malloc(size * sizeof(int));
+	sorted_arr = malloc(size * sizeof(int));
 
-	for (int i = size - 1; i >= 0; i--)
+	for (j = size - 1; j >= 0; j--)
 	{
-		sorted_arr[count_array[array[i]] - 1] = array[i];
-		count_array[array[i]]--;
+		sorted_arr[count_array[array[j]] - 1] = array[j];
+		count_array[array[j]]--;
 	}
-	for (size_t i = 0; i < size; i++)
+	for (i = 0; i < size; i++)
 		array[i] = sorted_arr[i];
 
 	free(count_array);
