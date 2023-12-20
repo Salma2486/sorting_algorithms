8ff7086b19f0fa763d6eb6b09ee35bc6b944ba1f
Younisprogramming <younistaher12341@gmail.com>
2023-12-19 16:39:26 -0800

    task 102

--- sort.h
+++ sort.h
@@ -33,4 +33,6 @@ void bitonic_merge(int *array, size_t size, size_t start, size_t seq,
 		char flow);
 void bitonic_seq(int *array, size_t size, size_t start, size_t seq, char flow);
 void bitonic_sort(int *array, size_t size);
+void counting_sort(int *array, size_t size);
+void merge_sort(int *array, size_t size);
 #endif
